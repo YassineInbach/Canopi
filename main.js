fbda9e43193a2a9adcd177dd133e09581507b1a7
YassineInbach <yassine.inbach@gmail.com>
2024-04-11 19:17:35 +0000

    Web Canopi

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
-const productContainers = document.querySelectorAll('.swiper-card');
-const nxtBtn = document.querySelectorAll('.next-btn');
-const preBtn = document.querySelectorAll('.prev-btn');
-const swiperBtns = document.querySelectorAll('.swiper-btn button');
+const dropdownToggle = document.getElementById('dropdownToggle');
+const dropdownMenu = document.getElementById('dropdownMenu');
 
-productContainers.forEach((item, i) => {
-    const images = item.querySelectorAll('.swiper-img');
-    const imageWidth = images[0].offsetWidth;
-
-    nxtBtn[i].addEventListener('click', () => {
-        const scrollAmount = item.scrollLeft + imageWidth;
-        item.scrollTo({
-            left: scrollAmount,
-            behavior: 'smooth'
-        });
-    });
-
-    preBtn[i].addEventListener('click', () => {
-        const scrollAmount = item.scrollLeft - imageWidth;
-        item.scrollTo({
-            left: scrollAmount >= 0 ? scrollAmount : 0,
-            behavior: 'smooth'
-        });
-    });
-
-    swiperBtns.forEach(btn => {
-        btn.addEventListener('click', () => {
-            const direction = btn.classList.contains('next-btn') ? 1 : -1;
-            const scrollAmount = item.scrollLeft + direction * imageWidth;
-            item.scrollTo({
-                left: scrollAmount,
-                behavior: 'smooth'
-            });
-        });
-    });
+dropdownToggle.addEventListener('click', function(e) {
+    e.preventDefault();
+    dropdownMenu.classList.toggle('show');
+    console.log("show")
 });
+
+$(".responsive").slick({
+    dots: true,
+    prevArrow: $(".prev"),
+    nextArrow: $(".next"),
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+     
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+
+   
